3938744ca6ab79cdcec739c57cb09f429880f181
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-01-29 09:49:09 -0500

    Skeleton for notifications

--- chat.js
+++ chat.js
@@ -1,12 +1,19 @@
 $(function () {
+  var manotify = false;
+  var notify = false;
   var socket = io();
   $('#send').submit(function(){
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
+    
     return false;
   });
   socket.on('chat message', function(msg){
     $('#messages').append($('<li>').text(msg));
+    if (notify) {
+      notify = manotify;
+      alert(msg);
+    }
     window.scrollTo(0, document.body.scrollHeight);
   });
   
