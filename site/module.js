e2073f321a6ceb088ddbcf4251ce29c93fae8ad8
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-02-28 11:02:22 -0500

    Insert site routes

--- site/module.js
+++ site/module.js
@@ -1,3 +1,11 @@
 module.exports = (app) => {
-
+  app.get("/site.js", (req, res) => {
+    res.sendFile(__dirname + "/site/main.js");
+  });
+  app.get("/site.css", (req, res) => {
+    res.sendFile(__dirname + "/site/styles.css");
+  });
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/site/index.html");
+  });
 };
