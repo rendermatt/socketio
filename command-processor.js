157ab86f5d8f03a8cb08a02833790e5bd63cea35
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-10-22 11:46:39 -0400

    wdym

--- command-processor.js
+++ command-processor.js
@@ -1,5 +1,5 @@
 const main = module.exports = (msg, from) => {
 	if (msg.startsWith("/")) {
-        from.emit("chat message", `Command detected! ${msg.slice(1).split(" ").join(",");}`);
+        from.emit("chat message", `Command detected! ${msg.slice(1).split().join(",");}`);
     }
 }
