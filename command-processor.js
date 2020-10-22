54577073e3284ed9ba85708d50e61d244b47f820
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-10-22 11:57:03 -0400

    This is a little embarrasing

--- command-processor.js
+++ command-processor.js
@@ -1,7 +1,7 @@
 const main = module.exports = (msg, from) => {
 	if (msg.startsWith("/")) {
       const args = msg.slice(1).split(" ");
-      const cmd = msg.unshift();
+      const cmd = msg.shift();
       from.emit("chat message", `Command detected! ${cmd}:${args}`);
     }
 }
