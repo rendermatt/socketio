c305bf0f8ff25e0edbcee3a9893eab7900ff34ca
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-10-22 11:58:51 -0400

    r/facepalm

--- command-processor.js
+++ command-processor.js
@@ -1,7 +1,7 @@
 const main = module.exports = (msg, from) => {
 	if (msg.startsWith("/")) {
       const args = msg.slice(1).split(" ");
-      const cmd = msg.shift();
+      const cmd = args.shift();
       from.emit("chat message", `Command detected! ${cmd}:${args}`);
     }
 }
