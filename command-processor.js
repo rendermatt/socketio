95f110419464344f7ec558a315dba09585aa23c9
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-10-23 08:50:17 -0400

    Add a test command, I'm scared

--- command-processor.js
+++ command-processor.js
@@ -1,7 +1,16 @@
+const {mes} = require("iomodule");
+const cdict = {};
 const main = module.exports = (msg, from) => {
-	if (msg.startsWith("/")) {
-      const args = msg.slice(1).split(" ");
-      const cmd = args.shift();
-      from.emit("chat message", `Command detected! ${cmd}:${args}`);
+  if (msg.startsWith("/")) {
+    const args = msg.slice(1).split(" ");
+    const cmd = args.shift();
+    from.emit("chat message", `Command detected! ${cmd}:${args}`);
+    switch(cmd) {
+      case "funpie":
+        mes(from, "cmdresp", `${args[0]} and ${args[1]} are stinky!`); return true;
+      default: mes(from, "cmdresp", `Unrecognized command ${cmd}. Run /help for help.`); re
     }
+    return true;
+  }
+  return false;
 }
