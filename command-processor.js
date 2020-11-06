54122933cd6ec6ab9c183470669822249a3c86fd
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-11-06 08:17:40 -0500

    Update command-processor.js

--- command-processor.js
+++ command-processor.js
@@ -1,4 +1,5 @@
 const cdict = {};
+const silent = {missing:false,handled:true}
 let mes = null;
 const main = module.exports = (_mes) => (msg, from) => {
   mes = _mes;
@@ -8,12 +9,12 @@ const main = module.exports = (_mes) => (msg, from) => {
     from.emit("chat message", `Command detected! ${cmd}:${args}`);
     switch(cmd) {
       case "funpie":
-        mes(from, "cmdresp", `${args[0]} and ${args[1]} are stinky!`); return true;
+        mes(from, "cmdresp", `${args[0]} and ${args[1]} are stinky!`); return silent.handled;
       
       case "tellraw":
-        mes(from, "none", args.join(" ")); return true;
+        mes(from, "none", args.join(" ")); return silent.handled;
       default:
-        mes(from, "cmdresp", `Unrecognized command ${cmd}. Run /help for help.`); return true;
+        mes(from, "cmdresp", `Unrecognized command ${cmd}. Run /help for help.`); return silent.missing;
     }
     return true;
   }
