bfba25617c9ed384e9b0ae586a61b276138e6ebd
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-11-06 08:43:34 -0500

    ah yes, _debug_commamd_detection

--- command-processor.js
+++ command-processor.js
@@ -5,7 +5,7 @@ const main = module.exports = (_mes) => (msg, from) => {
   if (msg.startsWith("/")) {
     const args = msg.slice(1).split(" ");
     const cmd = args.shift();
-    if(from._debug_commamd_detection) {from.emit("chat message", `Command detected! ${cmd}:${args}`)};
+    if(from._debug_command_detection) {from.emit("chat message", `Command detected! ${cmd}:${args}`)};
     switch(cmd) {
       case "funpie":
         mes(from, "cmdresp", `${args[0]} and ${args[1]} are stinky!`); return true;
@@ -13,7 +13,7 @@ const main = module.exports = (_mes) => (msg, from) => {
       case "tellraw":
         mes(from, "none", args.join(" ")); return true;
       case "_debug_command_detection_enable":
-        from._debug_commamd_detection = true; return true;
+        from._debug_command_detection = true; return true;
       default:
         mes(from, "cmdresp", `Unrecognized command ${cmd}. Run /help for help.`); return true;
     }
