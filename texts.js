f8979de9708283976609d1a62510d6de59a16778
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-11-23 10:36:47 -0500

    PARENTSSSSSSSS

--- texts.js
+++ texts.js
@@ -5,7 +5,7 @@ module.exports = {
     "join_self": (a,b)=>`Welcome, ${a} (${b})`,
     "nick": (a,b)=>`${a} has applied name ${b}.`,
     "nick_self": a=>`Name ${a} applied successfully.`,
-    "message": (a,b,c,d)=>`${c}${a.toString().padStart(2, "0")}:${b.toString.padStart(2, "0")} | ${d}`,
+    "message": (a,b,c,d)=>`${c}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
     "chat": (a,b)=>`<${a}> ${b}`,
   }
 };
