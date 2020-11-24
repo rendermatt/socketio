a4e4bf0579b4dc65bb53c8b0c20a04850ddffed1
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-11-24 11:00:23 -0500

    Format, send hello message to client and await response

--- iomodule.js
+++ iomodule.js
@@ -1,62 +1,60 @@
-
 const r = {};
 const LANG = "en_us";
 module.exports = {};
-
 r.io = null;
 r.pf = require("./prefixes.js");
 r.t = require("./texts.js")[LANG];
 const names = {};
-
-
 const rnames = {};
-const mes = (who, prefix, msg) => {var d = new Date(); who.emit("chat message", r.t.message((d.getHours()+8+12)%24, d.getMinutes(), r.pf[prefix], msg));};
-
+const mes = (who, prefix, msg) => {
+  var d = new Date();
+  who.emit("chat message", r.t.message((d.getHours() + 8 + 12) % 24, d.getMinutes(), r.pf[prefix], msg));
+};
 const ipToSocket = {};
-
-r.names = names; r.rnames = rnames;
+r.names = names;
+r.rnames = rnames;
 module.exports.r = r;
-
 const magic = module.exports.magic = (sender, msg) => {
-  if(r.cmdmod(msg, sender, sender)){return true;}
+  if (r.cmdmod(msg, sender, sender)) {
+    return true;
+  }
   switch (msg) {
-    
     //case "/iam Freshdude":
     //  apply_name(sender, "DarkWolf129"); return true;
     case "/iam Adam":
       //sender.disconnect(); return true;
-      apply_name(sender, "Azandfer"); return true;
-    //case "/iam pokepat12":
-    //  apply_name(sender, "Poképat12"); return true;
+      apply_name(sender, "Azandfer");
+      return true;
+      //case "/iam pokepat12":
+      //  apply_name(sender, "Poképat12"); return true;
     case "/imnot":
-      names[sender.id] = sender.id.slice(0,8);
-      mes(sender, "cmdresp", `You are now annonymous.`); return true;
+      names[sender.id] = sender.id.slice(0, 8);
+      mes(sender, "cmdresp", `You are now annonymous.`);
+      return true;
     case "":
       return true;
     case "/moo":
-      mes(sender, "cmdresp", `There are no easter eggs in this program.`); return true;
-    //case "/_debug_dump_naming":
-    //  mes(sender, "cmdresp", `names: ${JSON.stringify(names)}\nrnames: ${JSON.stringify(rnames)}`);
+      mes(sender, "cmdresp", `There are no easter eggs in this program.`);
+      return true;
+      //case "/_debug_dump_naming":
+      //  mes(sender, "cmdresp", `names: ${JSON.stringify(names)}\nrnames: ${JSON.stringify(rnames)}`);
     default:
       if (msg.startsWith("/iam")) return true;
       return false;
   }
 };
-
-
 const format_msg = module.exports.format_msg = msg => msg.replace("\\\\", "\f") // temp rm \\
-                                                         .replace("\\r\\n", "\n")
-                                                         .replace("\\r", "\\n")
-                                                         .replace("\\n", "<br/>")
-                                                         .replace("\\t", "\t")
-                                                         .replace("\f", "\\\\")
-                                                         .replace(/ass+ /i, "but")
-                                                         .replace(/fuck/i, "truck")
-                                                         .replace(/shit/i, "ship")
-                                                         .replace(/bitch/i, "female dog")
-                                                         .replace(/shut up/i, "shut down")
-                                                         .split("<br/>");
-
+  .replace("\\r\\n", "\n")
+  .replace("\\r", "\\n")
+  .replace("\\n", "<br/>")
+  .replace("\\t", "\t")
+  .replace("\f", "\\\\")
+  .replace(/ass+ /i, "but")
+  .replace(/fuck/i, "truck")
+  .replace(/shit/i, "ship")
+  .replace(/bitch/i, "female dog")
+  .replace(/shut up/i, "shut down")
+  .split("<br/>");
 module.exports.main = (io) => {
   r.io = io;
   r.cmdmod = require("./command-processor.js")(mes);
@@ -65,26 +63,31 @@ module.exports.main = (io) => {
     client.ipAddress = io.request.connection.remoteAddress;
     next();
   });*/
-  io.on("connection", (socket)=>{
-  socket.on('authenticate', (session)=>{
-    if (!session) socket.emit("authenticate", session = socket.id);
-    socket._id = socket.id; socket.id = /*session ? session :*/ socket.id;
-    names[socket.id] = socket.id.slice(0,8);
-    rnames[names[socket.id]] = socket;
-    mes(socket, "alert", r.t.join_self(names[socket.id], session));
-    mes(socket.broadcast, "alert", r.t.join(names[socket.id]));
-    socket.on("chat message", msg => console.log(`[CHAT ${names[socket.id]}] ${msg}`)); // who doesn't love log spam
-    socket.on('chat message', msg => (
-                                     magic(socket, msg) ?
-                                     undefined :
-                                     format_msg(msg).map((m) => {mes(io, "msg", r.t.chat(names[socket.id], m));})
-                                     ));
-    socket.on("disconnect", () => {
-      mes(socket.broadcast, "alert", r.t.leave(names[socket.id]));
-      //whoDisBot.onLeave(socket);
-      delete rnames[names[socket.id]];
-      names[socket.id] = undefined;
+  io.on("connection", (socket) => {
+    socket.on('hello', (session) => {
+      if (!session) socket.emit("authenticate", session = socket.id);
+      socket._id = socket.id;
+      socket.id = /*session ? session :*/ socket.id;
+      names[socket.id] = socket.id.slice(0, 8);
+      rnames[names[socket.id]] = socket;
+      mes(socket, "alert", r.t.join_self(names[socket.id], session));
+      mes(socket.broadcast, "alert", r.t.join(names[socket.id]));
+      socket.on("chat message", msg => console.log(`[CHAT ${names[socket.id]}] ${msg}`)); // who doesn't love log spam
+      socket.on('chat message', msg => (
+        magic(socket, msg) ?
+        undefined :
+        format_msg(msg)
+        .map((m) => {
+          mes(io, "msg", r.t.chat(names[socket.id], m));
+        })
+      ));
+      socket.on("disconnect", () => {
+        mes(socket.broadcast, "alert", r.t.leave(names[socket.id]));
+        //whoDisBot.onLeave(socket);
+        delete rnames[names[socket.id]];
+        names[socket.id] = undefined;
+      });
     });
-    });
+    setTimeout(()=>socket.emit("hello"), 250);
   });
 };
