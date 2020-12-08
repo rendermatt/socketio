ea1186f872c52620f64b01b74225363f730c6da8
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-12-08 11:56:12 -0500

    Let texts.js use the parameters provided from that commit

--- texts.js
+++ texts.js
@@ -1,11 +1,11 @@
-module.exports = {
+module.exports = (r) => ({
   "en_us": {
     "join": a=>`<${a}> has joined.`,
     "leave": a=>`<${a}> has left. T`,
     "join_self": (a,b)=>`Welcome, ${a} (${b})`,
     "nick": (a,b)=>`${a} has applied name ${b}.`,
     "nick_self": a=>`Name ${a} applied successfully.`,
-    "message": (a,b,c,d)=>`${c}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
+    "message": (a,b,c,d)=>`${(r.pf[c])?(r.pf[c]):("("+c+")")}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
     "chat": (a,b)=>`<${a}> ${b}`,
   }
-};
+});
