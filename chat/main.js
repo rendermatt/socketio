2ff2e9e6d8961d8959558e05457bb150d890dab4
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-01-12 16:56:31 -0500

    Replace it with the JSFiddle code

--- chat/main.js
+++ chat/main.js
@@ -31,20 +31,27 @@ $(function () {
   socket.on("reload", ()=>{history.go(0);});
   socket.on("linkout", (url)=>{open(url);});
   $.on("blur", ()=>{alert("blur");});
-  document.getElementById("m").onpaste = (event) => {
-    alert("pasted");
-    var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-    alert(JSON.stringify(items)); // will give you the mime types
-    for (index in items) {
-      var item = items[index];
-      if (item.kind === 'file') {
-        var blob = item.getAsFile();
-        var reader = new FileReader();
-        reader.onload = function(event){
-        socket.emit("image", event.target.result)}; // data url!
-        reader.readAsDataURL(blob);
+  document.getElementById('m').onpaste = function (event) {
+  // use event.originalEvent.clipboard for newer chrome versions
+  var items = (event.clipboardData  || event.originalEvent.clipboardData).items;
+  console.log(JSON.stringify(items)); // will give you the mime types
+  // find pasted image among pasted items
+  var blob = null;
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type.indexOf("image") === 0) {
+      blob = items[i].getAsFile();
     }
   }
+  // load image if there is a pasted image
+  if (blob !== null) {
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      console.log(event.target.result); // data url!
+      document.getElementById("pastedImage").src = event.target.result;
+    };
+    reader.readAsDataURL(blob);
+  }
+}
 }
 
 });
