363da4982fa18c8f8cbdd22aa35398332653487c
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-01-15 08:07:41 -0500

    Underp the escapes

--- texts.js
+++ texts.js
@@ -1,11 +1,11 @@
 module.exports = (r) => ({
   "en_us": {
-    "join": a=>`&lt;${a}&rt; has joined.`,
-    "leave": a=>`&lt;${a}&rt; has left. T`,
+    "join": a=>`&lt;${a}&gt; has joined.`,
+    "leave": a=>`&lt;${a}&gt; has left. T`,
     "join_self": (a,b)=>`Welcome, ${a} (${b})`,
     "nick": (a,b)=>`${a} has applied name ${b}.`,
     "nick_self": a=>`Name ${a} applied successfully.`,
     "message": (a,b,c,d,e)=>`${e.padStart(4, "0")}: ${(r.pf[c])?(r.pf[c]):("("+c+") ")}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
-    "chat": (a,b)=>`&lt;${a}&rt; ${b}`,
+    "chat": (a,b)=>`&lt;${a}&gt; ${b}`,
   }
 });
