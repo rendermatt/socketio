ef7f59628bd3963af76440eda13415cb4f379a2a
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-01-15 08:05:20 -0500

    Escape the lgt signs

--- texts.js
+++ texts.js
@@ -1,11 +1,11 @@
 module.exports = (r) => ({
   "en_us": {
-    "join": a=>`<${a}> has joined.`,
-    "leave": a=>`<${a}> has left. T`,
+    "join": a=>`&lt;${a}&rt; has joined.`,
+    "leave": a=>`&lt;${a}&rt; has left. T`,
     "join_self": (a,b)=>`Welcome, ${a} (${b})`,
     "nick": (a,b)=>`${a} has applied name ${b}.`,
     "nick_self": a=>`Name ${a} applied successfully.`,
     "message": (a,b,c,d,e)=>`${e}: ${(r.pf[c])?(r.pf[c]):("("+c+") ")}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
-    "chat": (a,b)=>`<${a}> ${b}`,
+    "chat": (a,b)=>`&lt;${a}&rt; ${b}`,
   }
 });
