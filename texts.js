52d882c375e67341d9a273240b2bcdcc2575cc95
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-01-15 08:11:52 -0500

    Stringifry prefix

--- texts.js
+++ texts.js
@@ -5,7 +5,7 @@ module.exports = (r) => ({
     "join_self": (a,b)=>`Welcome, ${a} (${b})`,
     "nick": (a,b)=>`${a} has applied name ${b}.`,
     "nick_self": a=>`Name ${a} applied successfully.`,
-    "message": (a,b,c,d,e)=>`${e.padStart(4, "0")}: ${(r.pf[c])?(r.pf[c]):("("+c+") ")}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
+    "message": (a,b,c,d,e)=>`${e.toString().padStart(4, "0")}: ${(r.pf[c])?(r.pf[c]):("("+c+") ")}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
     "chat": (a,b)=>`&lt;${a}&gt; ${b}`,
   }
 });
