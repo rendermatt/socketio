a1d03b6bbd98773e1a231071daac7d69fe113121
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-02-16 09:24:53 -0500

    Create loader "test"

--- chat/module.js
+++ chat/module.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-  app.get('/chat', function(req, res){
+  app.get('/test', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
-  app.get("/chat.js", (req, res) => {
+  app.get("/test.js", (req, res) => {
     res.sendFile(__dirname + "/main.js");
   });
-  app.get("/chat.css", (req, res) => {
+  app.get("/test.css", (req, res) => {
     res.sendFile(__dirname + "/styles.css");
   });
 }
