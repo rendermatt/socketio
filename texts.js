ded0ca050874a2baa597e017b932c0af2f6b1a0c
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-03-01 09:18:41 -0500

    Add action translation and rearrange stuff
    Remember to put message at the end, it messes up formatting despite it being perfectly valid

--- texts.js
+++ texts.js
@@ -5,9 +5,10 @@ module.exports = (r) => ({
     "kick": a=>`&lt;${a}&gt; was hit by the rubber boot!`,
     "ban": a=>`&lt;${a}&gt; was hit by the banhammer. :(`,
     "join_self": (a,b)=>`Welcome to the ${process.env.SERVER_NAME || "local"} server, ${a} (${b})`,
+    "chat": (a,b)=>`&lt;${a}&gt; ${b}`,
+    "action": (a,b)=>`* ${a} ${b}`,
     "nick": (a,b)=>`${a} has applied name ${b}.`,
     "nick_self": a=>`Name ${a} applied successfully.`,
     "message": (a,b,c,d,e,f='')=>`${f.toString().padStart(12, " ").slice(-12)} ${e.toString().padStart(6, "0").slice(-6)}: ${(r.pf[c])?(r.pf[c]):("("+c+") ")}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
-    "chat": (a,b)=>`&lt;${a}&gt; ${b}`,
   }
 });
