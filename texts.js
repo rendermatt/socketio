f00f2301b72ef5733d5bf90afb7ff46c75a1e724
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-03-12 09:04:31 -0500

    Update texts.js

--- texts.js
+++ texts.js
@@ -8,6 +8,11 @@ module.exports = (r) => ({
     "chat": (a,b)=>`&lt;${a}&gt; ${b}`,
     "action": (a,b)=>`* ${a} ${b}`,
     "nick": (a,b)=>`${a} has applied name ${b}.`,
+    "truly": {
+      "you": ()=>`Yes, you are yourself.`,
+      "kicky": (a)=>`You were told the truth by ${a}`,
+      "kick": (a,b)=>`${a} was told the truth about ${b}.`,
+    },
     "nick_self": a=>`Name ${a} applied successfully.`,
     "message": (a,b,c,d,e,f='')=>`${f.toString().padStart(12, " ").slice(-12)} ${e.toString().padStart(6, "0").slice(-6)}: ${(r.pf[c])?(r.pf[c]):("("+c+") ")}${a.toString().padStart(2, "0")}:${b.toString().padStart(2, "0")} | ${d}`,
   }
