060f72ee52e20bace5b98f8e433bd35ede34389e
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-03-15 12:05:38 -0400

    Add semicolons, close parents, and fix an unmatched curly-parent that existed for too long

--- chat/main.js
+++ chat/main.js
@@ -1,14 +1,14 @@
 $(function () {
-  if (localStorage.banned) window.location.path = "/banned"
+  if (localStorage.banned) window.location.path = "/banned";
   var manotify = false;
   var notify = false;
   var socket = io();
   socket.on("hello", ()=>{
-    $("#messages").append($('<li>', {class: "join"}).text(`Connected to NoMoreNotes at ${Date.now()}`);
+    $("#messages").append($('<li>', {class: "join"}).text(`Connected to NoMoreNotes at ${Date.now()}`));
     socket.emit("hello", localStorage.session ? localStorage.session : (localStorage.session = socket.id));
   });
   socket.on("disconnect", ()=>{
-    $("#messages").append($('<li>', {class: 'leave'}).text(`Lost connection to NoMoreNotes at ${Date.now()}`);
+    $("#messages").append($('<li>', {class: 'leave'}).text(`Lost connection to NoMoreNotes at ${Date.now()}`));
   $('#send').submit(function(){
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
@@ -60,3 +60,4 @@ $(function () {
   }
 };
 });
+  });
