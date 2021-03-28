fba6293f7565403a61247e6c401a8bd928c40ad3
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-03-28 11:43:19 -0400

    Add semis and fix the thing I think

--- surr.js
+++ surr.js
@@ -8,11 +8,11 @@ exports.unsurrogate = function (p2_idx, text) {
       }
     }
   return p1_idx;
-}
+};
 
 exports.surrogate = function (p1_idx, text) {
   var p2_idx = p1_idx;
-  for (var i = 0; i < text.length && i < js_idx; i++) {
+  for (var i = 0; i < text.length && i < p2_idx; i++) {
     var char_code = text.charCodeAt(i);
     // check for the first half of a surrogate pair
     if (char_code >= 0xD800 && char_code < 0xDC00) {
@@ -20,10 +20,10 @@ exports.surrogate = function (p1_idx, text) {
     }
   }
   return p2_idx;
-}
+};
 
 exports.issurrogate = function (text) {
   if(typeof text != typeof 'string') throw new TypeError(`expected ${typeof 'string'}, got ${typeof text}`); // require string
   var p1_idx = text.length;
   return exports.surrogate(p1_idx, text) != p1_idx;
-}
+};
