dbfe3d0beb55af91236c8c2605cb6c0f91811633
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-04-01 07:39:21 -0400

    Fine, I do this, is this fine?

--- site/main.js
+++ site/main.js
@@ -1,5 +1,5 @@
 // <style> * {display: inline-block;} </style>
-(() => {
+/*(() => {
   const prevFeed = $("#prev-feed")
   const sock = io();
   sock.on("chat message", (msg) => {
@@ -8,23 +8,27 @@
     }
     prevFeed.append($("<li>").text(msg)); // </li>
   });
-});
+});*/
 
-async function loadTheme() {
-  alert("loading theme");
-  fetch("/themes.json")
-    .then(data => {
-      alert("theme data fetched");
-      const ust  = readCookie("theme") || data._default_;
-      alert(`got user prefrences: ${ust}`);
-      const color= data[ust];
-      alert(`which means color ${color}`);
-      createCookie("theme", ust, 7);
-    });
-    .catch(err => {
-      alert(`could not load because ${err.type}: ${err.message}`);
-      alert(err.stack);
-    });
+function loadTheme() {
+  return new Promise((res, rej) => {
+    alert("loading theme");
+    fetch("/themes.json")
+      .then(data => {
+        alert("theme data fetched");
+        const ust  = readCookie("theme") || data._default_;
+        alert(`got user prefrences: ${ust}`);
+        const color= data[ust];
+        alert(`which means color ${color}`);
+        createCookie("theme", ust, 7);
+        res(color);
+      })
+      .catch(err => {
+        alert(`could not load because ${err.type}: ${err.message}`);
+        alert(err.stack);
+        rej(err);
+      });
+  });
 }
 
 $(()=> {
