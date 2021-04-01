c42cc599799b641764edf74c194bcae13d4f86d9
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-03-31 20:22:24 -0400

    Add BASIC theme support
    Also: IB the everything in case of HTMLification, disable the old prev_feed system (and rename variables)

--- site/main.js
+++ site/main.js
@@ -1,10 +1,22 @@
-$(() => {
-  const prev_feed = $("#prev-feed")
+// <style> * {display: inline-block;} </style>
+(() => {
+  const prevFeed = $("#prev-feed")
   const sock = io();
   sock.on("chat message", (msg) => {
-    if (prev_feed.children().size() >= 5) {
-      prev_feed.children().first().remove();
+    if (prevFeed.children().size() >= 5) {
+      prevFeed.children().first().remove();
     }
-    prev_feed.append($("<li>").text(msg));
+    prevFeed.append($("<li>").text(msg)); // </li>
   });
+});
+
+async function loadTheme() {
+  const data = JSON.parse(await fetch("/themes.json"));
+  const ust  = readCookie("theme") || data._default_;
+  const color= data[ust];
+  createCookie("theme", ust, 7);
+}
+
+$(()=> {
+  loadTheme();
 });
