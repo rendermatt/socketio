da5bad7dc86922c90bea5019a626af59e7d64979
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-06-07 11:08:45 -0400

    Make saveable editor work

---
+++ vis/main.js
@@ -0,0 +1,16 @@
+let selectDomElement;
+$(".select button").click(selectDomEvent = e => {
+	$(e.target.parentElement).find(".selected").removeClass("selected");
+  $(e.target).addClass("selected");
+  localStorage.NMNmode = $(e.target).attr("value");
+});
+
+$("input").on("input", e => {
+	if (e.target.value) localStorage.NMNname = e.target.value;
+  else localStorage.removeItem("NMNname");
+});
+
+$(_=>{
+	if (localStorage.NMNname) $("input").val(localStorage.NMNname);
+  selectDomEvent({target: $(`.select button[value=${localStorage.NMNmode ? localStorage.NMNmode : 0}]`)[0]});
+})
