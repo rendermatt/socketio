645e88e129eecdea27def02d46afd7d895582456
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2022-01-19 14:04:19 -0500

    Debracket messages

--- texts.js
+++ texts.js
@@ -1,9 +1,9 @@
 module.exports = (r) => ({
   "en_us": {
-    "join": a=>`&lt;${a}> has joined.`,
-    "leave": a=>`&lt;${a}> has ${Math.random() < .1 ? "right" : "left"}. T`,
-    "kick": (a,b)=>`&lt;${a}> was hit by &lt;${b}>'s rubber boot!`,
-    "ban": (a,b,c,d)=>`&lt;${a}>; was hit by &lt;${b}>'s ${c}-pound banhammer: ${d}`,
+    "join": a=>`${a} has joined.`,
+    "leave": a=>`${a} has ${Math.random() < .1 ? "right" : "left"}. T`,
+    "kick": (a,b)=>`${a} was hit by ${b}'s rubber boot!`,
+    "ban": (a,b,c,d)=>`${a} was hit by ${b}'s ${c}-pound banhammer: ${d}`,
     "join_self": (a,b)=>`Welcome to the ${process.env.SERVER_NAME || "local"} server, ${a} (${b})`,
     "chat": (a,b)=>`&lt;${a}> ${b}`,
     "action": (a,b)=>`* ${a} ${b}`,
