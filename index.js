03193bc2f51ff3979e5c0d4a76868b7434fd6f36
Carl Crozier <crozier83@googlemail.com>
2022-03-15 12:46:25 +0000

    Small changes to index html and index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
+const badNamedVariable = "Yes";
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 });
 
