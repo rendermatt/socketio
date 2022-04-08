9d3395fd705c6ad470a461004ae2f9b56daf869e
Sergio Lopez <sergio@softnya.com>
2022-04-08 12:08:48 -0500

    Chat funcional sin DB

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ You can also spin up a free Heroku dyno to test it out:
 Or run it on [Repl.it](https://repl.it/):
 
 [![Run on Repl.it](https://repl.it/badge/github/socketio/chat-example)](https://repl.it/github/socketio/chat-example)
+
+https://socket.io/get-started/chat#getting-this-example
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/chat.html');
 });
 
 io.on('connection', (socket) => {
