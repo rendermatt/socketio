f727679a1fcc6ce384f189dd2d383e98c1eef161
PoolloverNathan <>
2022-10-13 17:54:09 +0000

    Make the database pretty

--- db.js
+++ db.js
@@ -4,13 +4,13 @@ const { promises: fsp } = require("fs")
 exports.data = data
 
 exports.save = () => {
-  fsp.writeFile("db.json", JSON.stringify(data))
+  fsp.writeFile("db.json", JSON.stringify(data, null, 2))
 }
 
 exports.touch = (key) => {
   if (key in data) {
     return data[key]
   } else {
-    return data[key] = {}
+    data[key] = {}
   }
 }
