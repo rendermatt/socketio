60147d18ce8ba578244e7766e065d3a662a9315d
dillen-hub <dillen99999999@gmail.com>
2022-10-30 18:41:34 +0800

    first

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const app = require('express')();
+const { socket } = require('server/router');
+
+const express = require('express')();
+const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
-
+app.use(express.static('public'))
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+    socket.on('sub_hit_count', hit => {
+        console.log(hit)
+        io.emit('sub_hit_count', hit.message1, hit.player_name);
+
+    });
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
