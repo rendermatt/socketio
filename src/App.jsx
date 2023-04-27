75e8486ea6f623488b93de93189d87a500a79e34
Gabriel Scocozza <ggustavoscocozza@shutterstock.com>
2023-04-27 17:36:27 -0300

    react and express

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import socketIO from "socket.io-client";
+
+const socket = socketIO.connect("http://localhost:4000");
 
 function App() {
   return (
